fadd743605d685d23dadfa6b2cdeb37f17e3e709
andy <urbanwhaleshark@gmail.com>
2012-12-31 01:27:34 +0000

    Pushed maps to dropbox

--- routes/bin.js
+++ routes/bin.js
@@ -15,12 +15,23 @@ function bin(app) {
       , core = require('../lib/core/core')
       , url = core.getUrlObj(req), obj = {}
       , data = core.jsonPath(library, '$..films[?(@.id===' + url.query.id + ')]')[0]
+      , filename = data.title
       , foundHexFile = app.static.hex[data.title]
+      , fileLocation = 'maps/' + filename + '.hex'
 
     if (foundHexFile) {
+      console.log('From cache.');
       res.send(foundHexFile)
     } else {
-      fs.readFile('./public/maps/' + data.title + '.hex', 'utf-8', function(err, data) {
+      app.dbox.get(fileLocation, function(status, data, metadata) {
+        if (status && status === 200) {
+          app.static.hex[filename] = data
+          res.send(data)
+        } else {
+          console.log(status,'Error loading hex file:', fileLocation);
+        }
+      })
+/*      fs.readFile('./public/maps/' + data.title + '.hex', 'utf-8', function(err, data) {
         if (err) {
           console.log(err)
           res.send(404)
@@ -29,7 +40,7 @@ function bin(app) {
           res.send(data)
         }
       });
-      
+*/      
     }
 
   }  
